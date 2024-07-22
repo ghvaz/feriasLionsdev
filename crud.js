c2eb4052b40368929d1595c95c213172f7d6765b
ghvaz <gh.vaz28@gmail.com>
2024-07-22 20:55:58 -0300

    crud

---
+++ crud.js
@@ -0,0 +1,92 @@
+const prompt = require('prompt-sync')({sigint: true})
+
+var CRUD = {
+    pesquisa: function(array, qual){
+        let onde = -1
+        array.forEach((element,index) => {
+            if(qual == element.dia){
+                onde = index
+            }
+        });
+        return onde
+    },
+
+    
+
+    add: function(array){
+        console.clear()
+        let dia = Number(prompt('Qual dia quer adicionar:'))
+        if(dia > 14 && dia < 20){
+            let feito = ''
+            if (this.pesquisa(array, dia) >= 0) {
+                console.log("Dia já cadastrado, adicione mais informações");
+                feito += prompt(`O que fez a mais no dia ${dia}:`) 
+                array[this.pesquisa(array, dia)].feitos += feito
+            }else{
+                feito = prompt(`O que fez no dia ${dia}:`)
+                array.push({dia, feito});
+            }
+            return;
+        }else{
+            let op = prompt(`Dia informado não esta no intervalo ( entre os dia 15 e 19 )\nEnter para continuar.\nOu sair para retornar o menu.`);
+            op = op.toLocaleLowerCase().toString().trim()
+            if(op !== 'sair'){
+                CRUD.add(array)
+            }
+            
+        }
+    },
+    read: function (array, read) {
+        console.clear()
+        array.forEach(element => {
+            console.log(`------------------------------------------------------------------
+            Dia: ${element.dia}:
+    Feitos:\n`);
+        element.feitos.forEach((elmt, i) => {
+            console.log((i+1)+". "+elmt)
+        });
+        });
+        console.log("\n");
+
+        if(read){
+            prompt("Enter para sair.")
+        }
+    },
+    update: function(array, bool){
+        CRUD.read(array, bool)
+        let qual = +prompt('Qual dia quer alterar os feitos:')
+        let index = CRUD.pesquisa(array,qual )
+        if(index >= 0){
+            console.log(array[index].feitos+"\n------------------------------------------------------------------\nAlterando");
+            array[index].feitos = prompt('altere os feitos:')
+            prompt("Alterado com sucesso.")
+        }else{
+            console.log(`Dia não encontrado.`);
+            let op = prompt("Enter para tentar novamente ou sair para voltar ao menu.")
+            op = op.toString().trim().toLocaleLowerCase()
+            if(op !== 'sair'){
+                CRUD.update(array, bool)
+            }
+        }
+    },
+    remove: function(array, bool){
+        CRUD.read(array, bool)
+        let qual =  prompt("Qual dia quer excluir: ")
+        let index = CRUD.pesquisa(array, qual)
+
+        if(index >=0){
+            array.slpice(index, 1)
+            prompt('Deletado com sucesso. Enter para retornar ao menu')
+        }else{
+            console.log(`Dia não encontrado.`);
+            let op = prompt("Enter para tentar novamente ou sair para voltar ao menu.")
+            op = op.toString().trim().toLocaleLowerCase()
+            if(op !== 'sair'){
+                CRUD.update(array, bool)
+            }
+        }
+    }
+}
+
+
+module.exports = CRUD
---
+++ index.js
@@ -0,0 +1,58 @@
+const prompt = require('prompt-sync')({sigint: true})
+const readline = require('readline')
+const crud = require('./crud.js')
+/*
+Faça um script de javascript que gerencia (CRUD) as atividades que o usuário fez nas férias, 
+o usuário informa o dia e as atividades que ele realizou no realizou no dia (pode ser mais de uma atividade por dia).
+
+Utilize todos os conceitos abaixo:
+
+Variáveis
+Array
+Objeto
+Condicionais (utilize tando switch, como if else)
+Laços de repetição (utilize forEach)
+Entrada e Saída
+Funções (Utilize function, arrow function e função anônima)
+Módulos (Toda a lógica referente às funções de CRUD e o armazenamento deve estar em um módulo separado do arquivo principal)
+Git/Github (Crie um repositório publico, e envie nesta atividade o link)
+NPM (Utilize o pacote prompt-sync para interagir com usuário)
+*/
+let ferias = [
+    {dia: 15, feitos:['trabalhei das 6:30 até as 15:20','Cheguei em casa as 16:20','Fiquei assistindo animes com minha esposa.']},
+    {dia: 16, feitos:['trabalhei das 6:30 até as 15:20','estudei pelo site do BeeCronwd']}
+]
+
+
+while (true) {
+    console.clear()
+    console.log(`
+1. adicionar uma atividade  diaria
+2.  leitura  das atividades diarias
+3.  alterar  uma adividade  diaria
+4.  remover  uma adividade  diaria
+
+0. SAIR DO PROGRAMA
+`);
+    let op = prompt('Digite uma das opções: ')
+    switch (op) {
+        case '1':
+            crud.add(ferias)
+            break;
+        case '2':
+            crud.read(ferias, true)
+            break;
+        case '3':
+            crud.update(ferias, false)
+            break;
+        case '4':
+            crud.remove(ferias, false)
+            break;
+        case '0':
+            process.exit();
+            break;
+        default:
+            prompt('Opção invalida. Enter para continuar')
+            break;
+    }
+}
